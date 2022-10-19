2ebd0e79c8b8e11240f60fca46d899aaaba3c9b5
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2022-10-19 16:52:39 -0300

    function on click button, increase and decrease cards

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,25 @@ function esconderCartaoSelecionado(){
 function mostrarCartao(indiceCartao) {
     cartoes[indiceCartao].classList.add("selecionado");
 }
+
+btnAvancar.addEventListener("click", function () {
+    if(cartaoAtual === cartoes.length -1) {
+        cartaoAtual = -1
+    }
+    
+    esconderCartaoSelecionado();
+    
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+    if(cartaoAtual === 0) {
+        cartaoAtual = (cartoes.length);
+    }
+    
+    esconderCartaoSelecionado();
+    
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+});
